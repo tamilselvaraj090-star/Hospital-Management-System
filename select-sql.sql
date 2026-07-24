b2c4426defe61f69444b3a31645ac33cc9ad984c
tamilselvaraj090-star <tamilselvaraj090@gmail.com>
2026-07-24 13:26:14 +0530

    Add files via upload

---
+++ select-sql.sql
@@ -0,0 +1,26 @@
+SELECT * FROM Patient;
+
+SELECT * FROM Doctor;
+
+SELECT * FROM Department;
+
+SELECT * FROM Staff;
+
+SELECT * FROM Appointment;
+
+SELECT * FROM Medicine;
+
+SELECT * FROM Prescription;
+
+SELECT * FROM Prescription_Details;
+
+SELECT * FROM Billing;
+
+SELECT * FROM Payment;
+
+SELECT * FROM Room;
+
+SELECT * FROM Nurse;
+
+SELECT * FROM Admission;
+
